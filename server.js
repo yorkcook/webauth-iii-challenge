52ab8fc481d2769a12054a445829016e2b86266c
York Cook <yorkcook@Johnny-Castle.local>
2019-08-21 17:10:34 -0400

    WIP: MVP

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 const authRouter = require("./auth/auth-router.js");
+const usersRouter = require("./users/users-router.js");
 
 const server = express();
 
@@ -12,6 +13,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send("It is AAALLLIIIVVVEEE!!!");
